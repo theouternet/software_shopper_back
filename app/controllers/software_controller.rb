bae8a30cc6740267f39a73db2f0ca39e1e4881d3
theouternet <jrougeau@gmail.com>
2020-07-16 20:41:12 -0500

    started #show

---
+++ app/controllers/software_controller.rb
@@ -0,0 +1,13 @@
+class SoftwareController < ApplicationController
+
+    #auth?
+
+BASEURL = 'https://api.bestbuy.com/v1/products'
+
+
+def show
+    softwareID = params[:id]
+    url = BASEURL + 
+
+    
+end 
