621f0960b1b14e1b70edc7eea67c85a4b90af4fa
theouternet <jrougeau@gmail.com>
2020-07-17 02:09:55 -0500

    #show img only

--- app/controllers/software_controller.rb
+++ app/controllers/software_controller.rb
@@ -7,7 +7,11 @@ BASEURL = 'https://api.bestbuy.com/v1/products'
 
 def show
     softwareID = params[:id]
-    url = BASEURL + 
+    API_KEY = ENV.fetch('API_KEY')
+    url = BASEURL + "(sku=" + softwareID + ")" + "?apiKey=" + API_KEY + "&show=image" + "&format=json"
+    response = RestClient.get("#{url}")
+    softwareParsed = JSON.parse(response)
 
+    render json: gameParsed
     
 end 
